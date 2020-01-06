2c6e6c39e5f0e209338e1c4457da086263e8743b
Sora DBH <47997682+duonghuan2122000@users.noreply.github.com>
2020-01-06 08:46:58 +0700

    add Project

---
+++ app/src/main/java/com/soradbh/firstkotlinappandroid/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.soradbh.firstkotlinappandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val initialTextViewTranslationY = textView_progress.translationY
+
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textView_progress.text = progress.toString()
+
+                val translationDistance = (initialTextViewTranslationY +
+                        progress * resources.getDimension(R.dimen.text_anim_step) * -1)
+
+                if(!fromUser){
+                    textView_progress.animate().translationY(translationDistance)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+
+        button_reset.setOnClickListener{v ->
+            seekBar.progress = 0
+        }
+    }
+}
